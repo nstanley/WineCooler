1e0d856ce001b77d9c19179228e68c204f27629e
nstanley <nstanley@users.noreply.github.com>
2017-08-21 19:07:13 -0500

    Separating UWP into MVC - threading, events, features, etc

--- Windows/WineCoolerUWP/WineCoolerUWP/MainPage.xaml.cs
+++ Windows/WineCoolerUWP/WineCoolerUWP/MainPage.xaml.cs
@@ -18,6 +18,8 @@ using Particle.SDK.RestApi;
 using Particle.SDK.Utils;
 using Windows.UI.Popups;
 using Windows.Security.Credentials;
+using Windows.System.Threading;
+using Windows.UI.Core;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -28,151 +30,111 @@ namespace WineCoolerUWP
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        //Available controllers
-        List<ParticleDevice> listParticleDevices;
-
-        //The particle controller
-        ParticleDevice pd_WineDevice;
-
-        bool loginSuccess = false;
-
-        //List of wine types
-        //Index matches arrays on Particle end
-        String[] listWineTypes = new String[] {"Storage", "Champagne", "Riesling", "Chardonnay", "Pinot Noir", "Cabernet Franc", "Syrah" };
-
-        //Options for serve duration
-        int[] listServeDurations = new int[] { 1, 2, 3, 4 };
-
-        String resourceName = "WineCoolerUWP";
-
+        WineManager wineFridge;
+        
         public MainPage()
         {
             this.InitializeComponent();
 
+            //Setup our manager and watch for events
+            wineFridge = new WineManager(Dispatcher);
+            wineFridge.LoginChangeEvent += new Action<bool>(UpdateLogin);
+            wineFridge.WineTempEvent += new Action<double>(UpdateTemperature);
+            wineFridge.AvailableDevicesEvent += new Action<List<ParticleDevice>>(UpdateDeviceList);
+            wineFridge.ExceptionEvent += new Action<Exception>(ShowException);
+            wineFridge.SystemModeEvent += new Action<int>(UpdateSystemMode);
+            wineFridge.DeviceChosenEvent += new Action<string>(UpdateDevice);
+
+            pvtFridge.IsEnabled = false;
+
             //ParticleCloud.SharedCloud.SynchronizationContext = System.Threading.SynchronizationContext.Current;
             
-            var loginCreds = GetCredentialFromLocker();
-            if(loginCreds != null)
-            {
-                loginCreds.RetrievePassword();
-                txtUserName.Text = loginCreds.UserName;
-                txtPassword.Password = loginCreds.Password;
-            }
-
-
             //Add wine types 1-6 (item 0 is Storage temp, not for serving)
-            for (int i = 1; i <= 6; ++i)
+            for (int i = 1; i < wineFridge.listWineTypes.Length; ++i)
             {
-                cboWineType.Items.Add(listWineTypes[i]);
+                cboWineType.Items.Add(wineFridge.listWineTypes[i]);
             }
-            cboWineType.SelectedItem = cboWineType.Items[3];
+            //cboWineType.SelectedItem = cboWineType.Items[3];
 
             //Add serving durations
-            for(int i = 0; i < 4; ++i)
+            for(int i = 0; i < wineFridge.listServeDurations.Length; ++i)
             {
-                cboDuration.Items.Add(listServeDurations[i]);
+                cboDuration.Items.Add(wineFridge.listServeDurations[i]);
             }
-            cboDuration.SelectedItem = cboDuration.Items[1];
+            //cboDuration.SelectedItem = cboDuration.Items[4];
+
+            //Add system modes
+            for (int i = 0; i < wineFridge.listModes.Length; ++i)
+            {
+                cboMode.Items.Add(wineFridge.listModes[i]);
+            }
+            //cboMode.SelectedItem = cboMode.Items[0];
         }
 
-        private async void btnLogin_Click(object sender, RoutedEventArgs e)
+        #region WineManager Events
+        private void UpdateLogin(bool success)
         {
-            try
-            {
-                loginSuccess = await ParticleCloud.SharedCloud.LoginAsync(txtUserName.Text, txtPassword.Password);
-            }
-            catch(Exception ex)
-            {
-                var md = new MessageDialog("Login failed!");                
-                await md.ShowAsync();
-            }
-            if(loginSuccess)
-            {
-                //Save login
-                var vault = new PasswordVault();
-                vault.Add(new PasswordCredential(resourceName, txtUserName.Text, txtPassword.Password));
+            cboDevice.IsEnabled = success;
 
-                //Get device list from particle cloud
-                listParticleDevices = await ParticleCloud.SharedCloud.GetDevicesAsync();
-                int i = 0;
-                foreach (ParticleDevice device in listParticleDevices)
-                {
-                    cboDevice.Items.Add(device.Name);
-                    if(device.Name == "WineCooler")
-                    {
-                        cboDevice.SelectedItem = cboDevice.Items[i];
-                    }
-                    ++i;
-                }
+            txtUserName.Text = wineFridge.loginUser;
+            txtPassword.Password = wineFridge.loginPass;
+        }
+
+        private void UpdateTemperature(double temp)
+        {
+            lblTemperature.Text = "Temperature: " + Math.Round(temp, 2).ToString() + "°C";
+        }
+
+        private void UpdateDeviceList(List<ParticleDevice> lpd)
+        {
+            foreach (ParticleDevice device in lpd)
+            {
+                cboDevice.Items.Add(device.Name);
             }
         }
 
-        private PasswordCredential GetCredentialFromLocker()
+        private async void ShowException(Exception ex)
         {
-            PasswordCredential credential = null;
-
-            var vault = new PasswordVault();
-            try
-            {
-                var credentialList = vault.FindAllByResource(resourceName);
-                if (credentialList.Count > 0)
-                {
-                    credential = credentialList[0];
-                }
-            }
-            catch(Exception ex)
-            {
-                //probably didn't have a stored credential
-            }
-            return credential;
+            var md = new MessageDialog("An issue has occurred!", "Uh oh...");
+            await md.ShowAsync();
         }
 
-        private async void btnDevice_Click(object sender, RoutedEventArgs e)
+        private void UpdateSystemMode(int mode)
         {
-            pd_WineDevice = await ParticleCloud.SharedCloud.GetDeviceAsync(cboDevice.SelectedItem.ToString());
+            cboMode.SelectedIndex = mode;
         }
 
-        private async void btnWineSetup_Click(object sender, RoutedEventArgs e)
+        
+        private void UpdateDevice(string name)
         {
-            try
-            {
-                String TypeFormat = (cboWineType.SelectedIndex + 1).ToString();
-                String TimeFormat = timePicker.Time.TotalMinutes.ToString() + "_" + ((int)cboDuration.SelectedIndex + 1).ToString();
-                ParticleFunctionResponse functionResponseType = await pd_WineDevice.RunFunctionAsync("SetWineType", TypeFormat);
-                ParticleFunctionResponse functionResponseTime = await pd_WineDevice.RunFunctionAsync("SetWineTime", TimeFormat);
-                int resultType = functionResponseType.ReturnValue;
-                int resultTime = functionResponseType.ReturnValue;
-                if (resultTime < 0 || resultType < 0)
-                {
-                    var md = new MessageDialog("There was an issue calling the functions!");
-                    await md.ShowAsync();
-                }
-                else
-                {
-                    var md = new MessageDialog("Wine Cooler Updated!");
-                    await md.ShowAsync();
-                }
-            }
-            catch(Exception ex)
-            {
-                var md = new MessageDialog("There was an issue calling the functions!");
-                await md.ShowAsync();
-            }
+            pvtFridge.IsEnabled = true;
+            cboDevice.SelectedIndex = cboDevice.Items.IndexOf(name);
+        }
+        #endregion
+
+        #region Button Events
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            wineFridge.Login(txtUserName.Text, txtPassword.Password);
         }
 
-        private async void btnRefreshTemp_Click(object sender, RoutedEventArgs e)
+        private void btnDevice_Click(object sender, RoutedEventArgs e)
         {
-            try
-            {
-                ParticleVariableResponse varResponse = await pd_WineDevice.GetVariableAsync("WineTemp");
-                double temp = (double)varResponse.Result;
-                lblTemperature.Text = "Temperature: " + temp.ToString() + "°C";
-            }
-            catch(Exception ex)
-            {
-                var md = new MessageDialog("There was an issue getting the temperature!");
-                await md.ShowAsync();
-            }
+            //pd_WineDevice = await ParticleCloud.SharedCloud.GetDeviceAsync(cboDevice.SelectedItem.ToString());
+        }
+
+        private void btnWineSetup_Click(object sender, RoutedEventArgs e)
+        {
+            String TypeFormat = (cboWineType.SelectedIndex + 1).ToString();
+            String TimeFormat = timePicker.Time.TotalMinutes.ToString() + "_" + ((int)cboDuration.SelectedIndex + 1).ToString();
+            wineFridge.WineSetup(TypeFormat, TimeFormat);
+        }
+
+        #endregion
+
+        private void cboMode_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            wineFridge.SetSystemMode(cboMode.SelectedItem.ToString());
         }
     }
 }
---
+++ Windows/WineCoolerUWP/WineCoolerUWP/WineManager.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Particle.SDK;
+using Windows.Security.Credentials;
+using Windows.System.Threading;
+using Windows.UI.Core;
+using Particle.SDK.Models;
+
+namespace WineCoolerUWP
+{
+    class WineManager
+    {
+        //Private properties
+        private ParticleDevice pd_WineController;
+        //private int serveMinutes;
+        private int systemMode;
+        private string resourceName = "WineManager";
+        private Exception uh_oh;
+        private CoreDispatcher UIDispatcher;
+
+        //Public properties
+        public bool loginSuccess { get; private set; }
+        public string loginUser { get; private set; }
+        public string loginPass { get; private set; }
+        public double temperature { get; private set; }
+        public List<ParticleDevice> listParticleDevices { get; private set; }
+        /*
+         * ENUM
+         */
+        public String[] listWineTypes = new String[] { "Storage", "Champagne", "Riesling", "Chardonnay", "Pinot Noir", "Cabernet Franc", "Syrah" };
+        public int[] listServeDurations = new int[] { 1, 2, 3, 4 };
+        public String[] listModes = new String[] { "Schedule", "Store", "Serve" };
+
+        //Events
+        public event Action<double> WineTempEvent;
+        public event Action<bool> LoginChangeEvent;
+        public event Action<List<ParticleDevice>> AvailableDevicesEvent;
+        public event Action<Exception> ExceptionEvent;
+        public event Action<bool> WineServeEvent;
+        public event Action<int> SystemModeEvent;
+        public event Action<string> DeviceChosenEvent;
+
+        //Constructor
+        public WineManager(CoreDispatcher dis)
+        {
+            UIDispatcher = dis;
+            //Login if possible (saved credentials
+            var loginCreds = GetCredentialFromLocker();
+            if (loginCreds != null)
+            {
+                loginCreds.RetrievePassword();
+                Login(loginCreds.UserName, loginCreds.Password);
+                loginUser = loginCreds.UserName;
+                loginPass = loginCreds.Password;
+            }
+        }
+
+        /// <summary>
+        /// Checks for the variable "WineTemp" attached to the WineManager's ParticleDevice and
+        /// assigns the temperature property.
+        /// Fires an event to let the UI know to read the new temperature
+        /// </summary>
+        /// <param name="timer"></param>
+        private async void ReadTempFromCloud(ThreadPoolTimer timer)
+        {
+            ParticleVariableResponse varResponse = await this.pd_WineController.GetVariableAsync("WineTemp");
+            temperature = (double)varResponse.Result;
+            WineTempEvent(temperature);
+        }
+
+        /// <summary>
+        /// Assigns a ParticleDevice to our manager
+        /// 
+        /// </summary>
+        /// <param name="pd"></param>
+        private void SetController(ParticleDevice pd)
+        {
+            pd_WineController = pd;
+            //Listen to Particle events to notify user when wine is servalble
+            Task<Guid> serveEventID = this.pd_WineController.SubscribeToDeviceEventsWithPrefixAsync(ServeHandler, "ServeReady");
+
+            //Check in on the temperature periodically
+            TimeSpan period = TimeSpan.FromSeconds(60);
+            ThreadPoolTimer PeriodicTimer = ThreadPoolTimer.CreatePeriodicTimer( async (source) =>
+            {
+                ParticleVariableResponse varResponse = await this.pd_WineController.GetVariableAsync("WineTemp");
+
+                temperature = (double)varResponse.Result;
+                await UIDispatcher.RunAsync(CoreDispatcherPriority.High,
+                    () =>
+                    {
+                        //
+                        // UI components can be accessed within this scope.
+                        //
+                        WineTempEvent(temperature);
+                    });
+            }
+            , period);
+
+            //Notify UI
+            DeviceChosenEvent(pd.Name);
+        }
+        
+        /// <summary>
+        /// Lets the UI know that we saw the "ServeReady" event come through the Particle cloud
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="particeEvent"></param>
+        private void ServeHandler(object sender, ParticleEventResponse particeEvent)
+        {
+            WineServeEvent(true);
+        }
+
+        /// <summary>
+        /// Attempts login to Particle Cloud. If successful, it (1) stores credentials, (2) gets device list and  lets UI know,
+        /// and (3) tries to assign a device if it includes "wine" in the name
+        /// </summary>
+        /// <param name="user">Email address of the user</param>
+        /// <param name="pass">Password of the user</param>
+        public async void Login(string user, string pass)
+        {
+            try
+            {
+                loginSuccess = await ParticleCloud.SharedCloud.LoginAsync(user, pass);
+            }
+            catch (Exception ex)
+            {
+                uh_oh = ex;
+                loginSuccess = false;
+                LoginChangeEvent(loginSuccess);
+                ExceptionEvent(uh_oh);
+            }
+            if (loginSuccess)
+            {
+                //Save login
+                PasswordVault passVault= new PasswordVault();
+                passVault.Add(new PasswordCredential(resourceName, user, pass));
+
+                //Get device list from particle cloud
+                listParticleDevices = await ParticleCloud.SharedCloud.GetDevicesAsync();
+
+                //Tell the UI
+                AvailableDevicesEvent(listParticleDevices);
+
+                //See if we can find a "wine" controller
+                //Sets the controlled device to first found with "wine" in the name
+                bool foundController = false;
+                foreach (ParticleDevice device in listParticleDevices)
+                {
+                    if (device.Name.IndexOf("wine", StringComparison.OrdinalIgnoreCase) >= 0 && !foundController)
+                    {
+                        foundController = true;
+                        this.SetController(device);
+                    }
+                }
+            }
+            else
+            {
+            }
+
+            //Let UI know to enable/disable functionality
+            LoginChangeEvent(loginSuccess);
+        }
+
+        public async void WineSetup(string type, string time)
+        {
+            try
+            {
+                
+                ParticleFunctionResponse functionResponseType = await pd_WineController.RunFunctionAsync("SetWineType", type);
+                ParticleFunctionResponse functionResponseTime = await pd_WineController.RunFunctionAsync("SetWineTime", time);
+                int resultType = functionResponseType.ReturnValue;
+                int resultTime = functionResponseType.ReturnValue;
+                if (resultTime < 0 || resultType < 0)
+                {
+                    //error from device
+                }
+                else
+                {
+                    //success
+                }
+            }
+            catch (Exception ex)
+            {
+                uh_oh = ex;
+                ExceptionEvent(uh_oh);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="mode"></param>
+        public async void SetSystemMode(string mode)
+        {
+            switch(mode)
+            {
+                case "Schedule":
+                    systemMode = 0;
+                    break;
+                case "Store":
+                    systemMode = 1;
+                    break;
+                case "Serve":
+                    systemMode = 2;
+                    break;
+                default:
+                    systemMode = 1;
+                    break;
+            }
+            try
+            {
+                ParticleFunctionResponse functionResponseTime = await pd_WineController.RunFunctionAsync("SetSysMode", systemMode.ToString());
+            }
+            catch(Exception ex)
+            {
+                uh_oh = ex;
+                ExceptionEvent(uh_oh);
+            }
+            SystemModeEvent(systemMode);
+        }
+
+        /// <summary>
+        /// Microsoft's example for getting stored credentials from a password vault
+        /// </summary>
+        /// <returns></returns>
+        private PasswordCredential GetCredentialFromLocker()
+        {
+            PasswordCredential credential = null;
+
+            var vault = new PasswordVault();
+            try
+            {
+                var credentialList = vault.FindAllByResource(resourceName);
+                if (credentialList.Count > 0)
+                {
+                    credential = credentialList[0];
+                }
+            }
+            catch (Exception ex)
+            {
+                uh_oh = ex;
+                ExceptionEvent(uh_oh);
+                //probably didn't have a stored credential
+            }
+            return credential;
+        }
+
+
+    }//end class
+}//end namesapce
